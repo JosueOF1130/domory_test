e04cd2255a044313c433ec552ea6130024d3eb4d
Josue Orozco <josue.orofre@gmail.com>
2026-03-06 22:10:08 -0800

    Initial Commit

---
+++ Domory Test/Domory_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Domory_TestApp.swift
+//  Domory Test
+//
+//  Created by Josue Orozco on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Domory_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
